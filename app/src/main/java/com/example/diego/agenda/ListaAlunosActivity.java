f31056a7da2eb95ec12035d0c7a37f78dbefd06b
DiegoPCNote <dto.diego@gmail.com>
2017-05-30 20:40:09 -0300

    Exericio 3 (Menu)

--- app/src/main/java/com/example/diego/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/diego/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package com.example.diego.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -18,5 +21,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         ListView listaAlunos = (ListView) findViewById(R.id.listaAlunos);
         listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intenteVaiParaOutraTela = new Intent(ListaAlunosActivity.this,FormularioActivity.class);
+                startActivity(intenteVaiParaOutraTela);
+            }
+        });
     }
 }
