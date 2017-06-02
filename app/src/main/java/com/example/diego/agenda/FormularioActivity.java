1cdf51fd857b51d29cbce39c36f9a46aa74eaac1
projeto_scsa <dto.diego@gmail.com>
2017-06-02 11:31:19 -0300

    Atividade 1 - Curso Alura II

--- app/src/main/java/com/example/diego/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/diego/agenda/FormularioActivity.java
@@ -52,7 +52,6 @@ public class FormularioActivity extends AppCompatActivity {
                 }
                 dao.close();
                 Toast.makeText(FormularioActivity.this,"Aluno " + aluno.getNome() + "Salvo!",Toast.LENGTH_SHORT).show();
-
                 finish();
                 break;
         }
--- app/src/main/java/com/example/diego/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/diego/agenda/ListaAlunosActivity.java
@@ -1,6 +1,10 @@
 package com.example.diego.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -20,6 +24,7 @@ import java.util.List;
 public class ListaAlunosActivity extends AppCompatActivity {
 
     ListView listaAlunos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,12 +70,54 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        //Botão Ligar
+        MenuItem ligar = menu.add("Ligar");
+        ligar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE}, 123);
+                } else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
+        //Botão ir para o site
+        MenuItem irParaSite = menu.add("Ir Para Site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+
+        String site = aluno.getSite();
+        if(site.startsWith("http://")||!site.startsWith("https://")){
+            site = "http://" + site;
+        }
+        intentSite.setData(Uri.parse(site));
+        irParaSite.setIntent(intentSite);
+
+        //Botão enviar sms
+        MenuItem sms = menu.add("Sms");
+        Intent intentSms = new Intent(Intent.ACTION_VIEW);
+        intentSms.setData(Uri.parse("sms:"+aluno.getTelefone()));
+        sms.setIntent(intentSms);
+
+        //Botão Vai para mapa
+        MenuItem vaiProMapa = menu.add("Vai Para Mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        vaiProMapa.setIntent(intentMapa);
+
+        //Botão Deletar
         MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
                 dao.close();
